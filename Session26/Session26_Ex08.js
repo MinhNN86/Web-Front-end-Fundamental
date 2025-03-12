e2ec8cc237464a85d31c7dcff0df7aa28de1315b
MinhNN86 <minhnguyennhatt@gmail.com>
2025-03-13 00:07:49 +0700

    update SS26_Ex08

--- Session26/Session26_Ex08.js
+++ Session26/Session26_Ex08.js
@@ -1,17 +1,28 @@
 let arr = [];
 let n = +prompt("Số phần tử mảng 10 -> 20");
 if (isNaN(n) || !Number.isInteger(n) || n < 10 || n > 20) {
-    alert("Dữ liệu không hợp lệ");
+  alert("Dữ liệu không hợp lệ");
 } else {
-    for(let i = 0; i < n;){
-        let input = +prompt(`Nhập phần tử thứ ${i + 1}`);
-        if (isNaN(input) || !Number.isInteger(input)) {
-            alert("Dữ liệu không hợp lệ");
-        } else {
-            arr.push(input);
-            i++;
-        }
+  for (let i = 0; i < n; ) {
+    let input = +prompt(`Nhập phần tử thứ ${i + 1}`);
+    if (isNaN(input) || !Number.isInteger(input)) {
+      alert("Dữ liệu không hợp lệ");
+    } else {
+      arr.push(input);
+      i++;
     }
-    let mangLe = arr.filter((el) => el % 2 !== 0);
-    alert(mangLe.length > 0 ? `${mangLe.join(", ")}` : "Mảng không có phần tử lẻ");
+  }
+  let mangLe = arr.filter((el) => el % 2 !== 0);
+  let mangLeNguyenTo = mangLe.filter((el) => {
+    if (el < 2) return false;
+    for (let i = 2; i <= Math.sqrt(el); i++) {
+      if (el % i === 0) return false;
+    }
+    return true;
+  });
+  alert(
+    mangLeNguyenTo.length > 0
+      ? `${mangLeNguyenTo.join(", ")}`
+      : "Mảng không có phần tử lẻ nguyên tố"
+  );
 }
